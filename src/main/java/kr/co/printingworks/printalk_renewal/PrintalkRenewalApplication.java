1b5174c982fb1325785e6c15cbf7d044ae33dc60
yskim621 <yskim62100@gmail.com>
2021-09-30 11:43:30 +0900

    Create printalk_renewal project with sprint_boot

---
+++ src/main/java/kr/co/printingworks/printalk_renewal/PrintalkRenewalApplication.java
@@ -0,0 +1,13 @@
+package kr.co.printingworks.printalk_renewal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintalkRenewalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintalkRenewalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/printingworks/printalk_renewal/PrintalkRenewalApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.printingworks.printalk_renewal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintalkRenewalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
